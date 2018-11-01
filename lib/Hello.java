71d991516e9a7a2842f99a9f184cce13a56b5a07
Henry W Ou <cs15xjl@its-cseb250-42.ucsd.edu>
2018-10-31 18:06:09 -0700

    Made interactive

--- lib/Hello.java
+++ lib/Hello.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Hello
 {
-
-// author
     public static void main( String argv[] )
     {
-        System.out.println( "Hello, World" + "!" );
+        System.out.println("What's your name");
+        Scanner in = new Scanner(System.in);
+        String my_name = in.nextLine();
+
+        System.out.println( "Hello, " + my_name + "!" );
     }
 }
+
